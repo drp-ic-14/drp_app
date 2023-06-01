6293549f625d74147c2472a99eb104b1c64fa2fe
Polycrylate <ahmedmahmud12971@gmail.com>
2023-06-01 20:06:15 +0100

    feat: add task list

---
+++ components/TaskItem.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CheckBox } from '@ui-kitten/components';
+import { Text, ListItem } from '@ui-kitten/components';
+
+type TaskItemProps = {
+    name: string,
+    location: string,
+    checked: boolean,
+}
+
+const TaskItem = (props: TaskItemProps): React.ReactElement => {
+
+  const [checked, setChecked] = React.useState(false);
+
+  const renderCheckBox = (): React.ReactElement => (
+    <CheckBox
+      checked={checked}
+      onChange={nextChecked => setChecked(nextChecked)}
+    />
+  );
+
+  const renderLocation = (): React.ReactElement => (
+    <Text>
+        {props.location}
+    </Text>
+  );
+
+  return (
+    <ListItem
+      title={`${props.name}`}
+      accessoryLeft={renderCheckBox}
+      accessoryRight={renderLocation}
+    />
+  );
+};
+
+export default TaskItem;
