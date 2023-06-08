b9be2b5f492db750ab99fbbb7ebb1e447bd5331a
Polycrylate <ahmedmahmud12971@gmail.com>
2023-06-08 13:49:09 +0100

    style: update eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['airbnb'],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//     // project: './tsconfig.json',
+//   },
+//   rules: {
+//   },
+// };
+
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    'react-hooks/exhaustive-deps': 'off',
+    root: true,
+    extends: ['@react-native-community', 'airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+    parserOptions: {
+        project: './tsconfig.json',
   },
 };
