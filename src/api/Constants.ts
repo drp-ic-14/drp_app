bb85441d6e4dc25078baaaf86cb3a4239fd7af21
Polycrylate <ahmedmahmud12971@gmail.com>
2023-06-09 02:26:00 +0100

    fix: back end url fix

--- src/api/Constants.ts
+++ src/api/Constants.ts
@@ -1,4 +1,5 @@
 import Config from 'react-native-config';
 
-export const { BACK_END_URL } = Config;
+// export const { BACK_END_URL } = Config;
+export const BACK_END_URL = 'https://drp-14-server.herokuapp.com';
 export const API_KEY = '123';
