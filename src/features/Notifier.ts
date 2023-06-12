f525cceea2c0d380c5e26a7c294d101a499a41d6
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-06-12 18:24:13 +0100

    feat: implement background listener for notification quick actions

--- src/features/Notifier.ts
+++ src/features/Notifier.ts
@@ -1,5 +1,29 @@
-import notifee from '@notifee/react-native';
+import notifee, { EventType } from '@notifee/react-native';
 import { Task } from '../utils/Interfaces';
+import { uuidAtom } from '../store/Atoms';
+import { deleteTask } from '../api/BackEnd';
+import { getRecoil } from 'recoil-nexus';
+
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  const { notification, pressAction } = detail;
+
+  if (type === EventType.ACTION_PRESS) {
+
+    switch (pressAction?.id) {
+      case 'mark-as-done': {
+        const data = notification?.data;
+        console.log(`deleting task '${data.taskId}' from user '${data.uuid}'`)    
+        await deleteTask(getRecoil(uuidAtom), data.taskId);
+
+        // Remove the notification
+        await notifee.cancelNotification(notification.id);
+      }
+      case 'remind-later': {
+        await notifee.cancelNotification(notification.id);
+      }
+    }
+  }
+});
 
 export async function notify(task: Task, distance: number) {
   console.log(`Notifying user about task '${task.name}'`);
@@ -11,6 +35,9 @@ export async function notify(task: Task, distance: number) {
 
     await notifee.displayNotification({
       title: `${task.name} - ${task.location} (${distance}m)`,
+      data: {
+        taskId: task.id
+      },
       android: {
         channelId,
         pressAction: {
