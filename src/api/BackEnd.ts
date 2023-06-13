59779174414278553665f13896909899eb2a744b
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-06-13 16:03:53 +0100

    feat: add helper functions for back-end api calls for checkId and createUser

--- src/api/BackEnd.ts
+++ src/api/BackEnd.ts
@@ -13,3 +13,34 @@ export const deleteTask = async (uuid: string, taskId: string) => {
     }),
   });
 }
+
+export const checkId = async (uuid: string): Promise<boolean> => {
+  const response = await fetch(`${BACK_END_URL}/api/check_id`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user_id: uuid,
+    }),
+  });
+  const exists = await response.json();
+  console.log(`check_id(${uuid}) => ${exists}`);
+  return exists;
+}
+
+export const createUser = async (uuid: string): Promise<void> => {
+  const response = await fetch(`${BACK_END_URL}/api/create_user`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user_id: uuid,
+    }),
+  });
+  if (response.ok) console.log(`Created new user '${uuid}'.`)
+  else console.error(`unable to create user ${uuid}`)
+}
