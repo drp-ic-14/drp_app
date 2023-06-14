f696639adbfb39eb9f5058fc45b6dfeb8853d103
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-06-14 14:32:08 +0100

    fix: change Constants.ts to contain correct back end links

--- src/api/Constants.ts
+++ src/api/Constants.ts
@@ -1,7 +1,7 @@
 import Config from 'react-native-config';
 
 // export const { BACK_END_URL } = Config;
-// export const BACK_END_URL = 'https://drp-14-server.herokuapp.com';
-export const BACK_END_URL = 'http://10.0.2.2:8000';
-export const BACK_END_WS = 'ws://10.0.2.2:8000';
+export const BACK_END_URL = 'https://drp-14-server.herokuapp.com';
+// export const BACK_END_URL = 'http://10.0.2.2:8000';
+export const BACK_END_WS = 'ws://drp-14-server.herokuapp.com';
 export const API_KEY = '123';
