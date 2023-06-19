fd027cf40905eb4912f7797b808e210205bcefad
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2023-06-19 17:55:30 +0100

    feat: add additional info to map help popup

--- src/components/MapHelp.tsx
+++ src/components/MapHelp.tsx
@@ -4,8 +4,8 @@ const MapHelp = () => {
   return (
     <View className="flex-0.8 bg-white absolute top-20 p-5 rounded-2xl">
       <View className="flex-row justify-between">
-        <Text className="text-slate-900 text-4xl">Help</Text>
-        <Text className="text-slate-400">Press '?' to close this.</Text>
+        {/* <Text className="text-slate-900 text-4xl">Help?</Text> */}
+        <Text className="text-slate-400">Press <Text className='font-extrabold'>'Help?'</Text> to close this.</Text>
       </View>
       <Text className="text-slate-900 text-lg">
         See all of your and your group(s) tasks on a map.
@@ -13,6 +13,9 @@ const MapHelp = () => {
       <Text className="text-slate-900 text-lg">
         Click on a marker to show the task's name, location and distance from
         you.
+      </Text>
+      <Text className="text-slate-900 text-lg">
+        Click the directions button in the bottom right to find the fastest way to your task using Google Maps.
       </Text>
     </View>
   );
